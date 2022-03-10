0c13deae93c1f519709aeabdd091b3b450c2da40
Nathan Shurtz <n.shurtz@fetchrewards.com>
2022-03-10 11:43:13 -0600

    change how websocket is constructed to utilize proper protocol ignore headers for now

--- apollo-runtime/src/jsMain/kotlin/com/apollographql/apollo3/network/ws/KtorWebSocketEngine.kt
+++ apollo-runtime/src/jsMain/kotlin/com/apollographql/apollo3/network/ws/KtorWebSocketEngine.kt
@@ -5,6 +5,9 @@ import io.ktor.client.engine.js.Js
 import io.ktor.client.features.websocket.WebSockets
 import io.ktor.client.features.websocket.webSocketSession
 import io.ktor.client.request.headers
+import io.ktor.client.request.url
+import io.ktor.http.HttpMethod
+import io.ktor.http.URLProtocol
 import io.ktor.http.Url
 import io.ktor.http.cio.websocket.CloseReason
 import io.ktor.http.cio.websocket.Frame
@@ -22,25 +25,47 @@ actual class DefaultWebSocketEngine(private val ktorClient: HttpClient) : WebSoc
       headers: Map<String, String>,
   ): WebSocketConnection = open(Url(url), headers)
 
-  private suspend fun open(url:Url, headers: Map<String, String>): WebSocketConnection {
-    val socketSession = ktorClient.webSocketSession(host = url.host, port = url.port, path = url.encodedPath) {
-      headers {
-        headers.forEach {
-          append(it.key, it.value)
+  private suspend fun open(url: Url, headers: Map<String, String>): WebSocketConnection {
+    val newUrl = url.copy(
+        protocol = when (url.protocol) {
+          URLProtocol.HTTPS -> URLProtocol.WSS
+          URLProtocol.HTTP -> URLProtocol.WS
+          URLProtocol.WS, URLProtocol.WSS -> url.protocol
+          /* URLProtocol.SOCKS */else -> throw UnsupportedOperationException("SOCKS is not a supported protocol")
+        })
+    println("url: $newUrl")
+    val socketSession = try {
+      ktorClient.webSocketSession {
+        url(newUrl)
+        method = HttpMethod.Get
+        headers {
+//          headers.forEach {
+//            println("key: ${it.key}, value:${it.value}")
+//            append(it.key, it.value)
+//          }
         }
       }
+    } catch (e: Throwable) {
+      println("stack: ${e.stackTraceToString()}")
+      throw e
     }
 
     return object : WebSocketConnection {
       override suspend fun receive(): String {
-        return socketSession.incoming.receive().data.decodeToString()
+        val received = socketSession.incoming.receive()
+        println("frame type: ${received.frameType}")
+        return received.data.decodeToString().also {
+          println(it)
+        }
       }
 
       override fun send(data: ByteString) {
+        println("send ${data.utf8()}")
         socketSession.outgoing.trySend(Frame.Binary(true, data.toByteArray()))
       }
 
       override fun send(string: String) {
+        println("send $string")
         socketSession.outgoing.trySend(Frame.Text(string))
       }
 
